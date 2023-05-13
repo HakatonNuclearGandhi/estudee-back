74c444eb0e06501b93704f1ba177ff7887218df7
VladTkach <70843124+VladTkach@users.noreply.github.com>
2023-05-13 19:14:32 +0300

    feature: added user domain model to project (#14)

---
+++ api/Models/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Models.Domain;
+
+public class User
+{
+    public Guid userId { get; set; }
+
+    public string userName { get; set; }
+
+    public string email { get; set; }
+
+    public string passwod { get; set; }
+}
