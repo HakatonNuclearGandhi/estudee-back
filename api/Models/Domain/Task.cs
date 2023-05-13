7340eb6472f64b68336fb5c208b18186e817d3ff
DenysMalanichev <58270142+DenysMalanichev@users.noreply.github.com>
2023-05-13 19:07:07 +0300

    feature: added task domain model (#13)

---
+++ api/Models/Domain/Task.cs
@@ -0,0 +1,12 @@
+namespace api.Models.Domain;
+
+public class Task
+{
+    public Guid taskId { get; set; }
+    public Guid subjectId { get; set; }
+    public string taskName { get; set; }
+    public int maxScore { get; set; }
+    public int score { get; set; }
+    public DateOnly deadLine { get; set; }
+    public Guid statusId { get; set; }
+}
