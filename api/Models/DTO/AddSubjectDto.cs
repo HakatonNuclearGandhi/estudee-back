11eca8981fd0fe124a921d93601703e450fc2189
Denys Malanichev <deniskirillov228@gmail.com>
2023-05-14 11:11:34 +0300

    feature: added get subject, subject name, add and update subject dtos

---
+++ api/Models/DTO/AddSubjectDto.cs
@@ -0,0 +1,8 @@
+namespace api.Models.DTO;
+
+public class AddSubjectDto
+{
+    public string subjectName { get; set; }
+    public string description { get; set; }
+    public int maxScore { get; set; }
+}
