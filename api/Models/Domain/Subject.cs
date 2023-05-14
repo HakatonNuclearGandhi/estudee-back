9addeebf70ea4a651b7b687f6ef5a518ecf9dc76
VladTkach <vladand2003@gmail.com>
2023-05-14 10:29:50 +0300

    feature: updated subject domain model

--- api/Models/Domain/Subject.cs
+++ api/Models/Domain/Subject.cs
@@ -3,9 +3,11 @@ namespace api.Models.Domain;
 public class Subject
 {
     public Guid subjectId { get; set; }
-    public Guid userId { get; set; }
     public string name { get; set; }
     public string description { get; set; }
     public int maxScore { get; set; }
     public int currentScore { get; set; }
+    
+    // Navigation properties
+    public User User { get; set; }
 }
