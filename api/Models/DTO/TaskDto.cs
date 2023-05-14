375ba122adf0b422f1c0e79e8db482b17d99298f
DenysMalanichev <58270142+DenysMalanichev@users.noreply.github.com>
2023-05-14 12:41:48 +0300

    fix: dateonly switched to datetime (#43)

--- api/Models/DTO/TaskDto.cs
+++ api/Models/DTO/TaskDto.cs
@@ -5,5 +5,5 @@ public class TaskDto
     public string subjectId { get; set; }
     public string taskName { get; set; }
     public int maxScore { get; set; }
-    public DateOnly deadline { get; set; }
+    public DateTime deadline { get; set; }
 }
