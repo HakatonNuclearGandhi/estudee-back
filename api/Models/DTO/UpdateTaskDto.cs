6939866b95d196e40ccfd2f842f75f0b2ce48ee5
Denys Malanichev <deniskirillov228@gmail.com>
2023-05-14 10:49:34 +0300

    fix: score and status id added to update task dto

--- api/Models/DTO/UpdateTaskDto.cs
+++ api/Models/DTO/UpdateTaskDto.cs
@@ -5,5 +5,7 @@ public class UpdateTaskDto
     public string subjectId { get; set; }
     public string taskName { get; set; }
     public int maxScore { get; set; }
+    public int score { get; set; }
+    public string statusId { get; set; }
     public DateOnly deadline { get; set; }
 }
