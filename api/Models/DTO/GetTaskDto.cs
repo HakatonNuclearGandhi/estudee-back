c7b0cbe5eb1c8f55052da545200dbc082692f0c0
Denys Malanichev <deniskirillov228@gmail.com>
2023-05-14 10:41:05 +0300

    fix: inheritance removed

--- api/Models/DTO/GetTaskDto.cs
+++ api/Models/DTO/GetTaskDto.cs
@@ -1,8 +1,12 @@
 namespace api.Models.DTO;
 
-public class GetTaskDto : TaskDto
+public class GetTaskDto
 {
     public string taskId { get; set; }
     public int score { get; set; }
     public string statusName { get; set; }
+    public string subjectId { get; set; }
+    public string taskName { get; set; }
+    public int maxScore { get; set; }
+    public DateOnly deadline { get; set; }
 }
