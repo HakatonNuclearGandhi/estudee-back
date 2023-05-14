69241a6b98a22bca57d7f9e138cba97246a174ed
VladTkach <vladand2003@gmail.com>
2023-05-14 11:27:43 +0300

    feature: added user and token DTOs

---
+++ api/Models/DTO/ResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace api.Models.DTO;
+
+public class ResponseDto 
+{
+    public string? Status { get; set; }
+    public string? Message { get; set; }
+}
---
+++ api/Models/DTO/TokenDto.cs
@@ -0,0 +1,7 @@
+﻿namespace api.Models.DTO;
+
+public class TokenDto
+{
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
